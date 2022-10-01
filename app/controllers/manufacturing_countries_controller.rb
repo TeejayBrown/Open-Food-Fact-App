a9208de443f6fc4d547bc1843aa6098554031b80
TeejayBrown <taiwo.omoleye@outlook.com>
2022-09-30 19:50:15 -0500

    Added the index and show views functionality to the Manufacturing Countries model.

--- app/controllers/manufacturing_countries_controller.rb
+++ app/controllers/manufacturing_countries_controller.rb
@@ -1,7 +1,9 @@
 class ManufacturingCountriesController < ApplicationController
   def index
+    @manufacturing_countries = ManufacturingCountry.all
   end
 
   def show
+    @manufacturing_country = ManufacturingCountry.find(params[:id])
   end
 end
